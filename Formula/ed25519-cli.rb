001b1100201ea0b56e067729c3255cd5c73e76ba
Jannik Arndt <jannik@jannikarndt.de>
2022-03-01 18:31:42 +0100

    Add formula

---
+++ Formula/ed25519-cli.rb
@@ -0,0 +1,19 @@
+class Ed25519Cli < Formula
+  desc "Command Line Interface for using ed25519 EdEC Crypto"
+  homepage "https://github.com/passulo/ed25519-cli/commits/main"
+  url "https://github.com/passulo/ed25519-cli/releases/tag/v0.1"
+  sha256 "81eea897e2e4d9c03e0766958285c32ff77e83754ff4c416e28ef49a804207a6"
+  license "Apache-2.0"
+
+  depends_on "openjdk@17" => :build
+  depends_on "sbt" => :build
+  
+  def install
+    system "sbt", "-sbt-jar", Formula["sbt"].opt_libexec/"bin/sbt-launch.jar", "build"
+    libexec.install "target/ed25519"
+  end
+
+  test do
+    system "#{bin}/ed25519 --version"
+  end
+end
