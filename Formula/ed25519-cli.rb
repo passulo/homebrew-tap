6430795a90451b0e8ba06ba623877f3fb6d18b1f
Jannik Arndt <jannik@jannikarndt.de>
2022-03-01 18:48:30 +0100

    Fix url

--- Formula/ed25519-cli.rb
+++ Formula/ed25519-cli.rb
@@ -1,7 +1,7 @@
 class Ed25519Cli < Formula
   desc "Command-Line Interface for using ed25519 EdEC Crypto"
-  homepage "https://github.com/passulo/ed25519-cli/commits/main"
-  url "https://github.com/passulo/ed25519-cli/releases/tag/v0.1"
+  homepage "https://github.com/passulo/ed25519-cli/"
+  url "https://github.com/passulo/ed25519-cli/archive/refs/tags/v0.1.tar.gz"
   sha256 "81eea897e2e4d9c03e0766958285c32ff77e83754ff4c416e28ef49a804207a6"
   license "Apache-2.0"
 
