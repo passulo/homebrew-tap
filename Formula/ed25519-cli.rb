3c145fb8b67e0b4162c69855b3e96f7a1f70b70d
Jannik Arndt <jannik@jannikarndt.de>
2022-03-01 20:52:24 +0100

    Use formular that copies executable

--- Formula/ed25519-cli.rb
+++ Formula/ed25519-cli.rb
@@ -1,17 +1,16 @@
 class Ed25519Cli < Formula
   desc "Command-Line Interface for using ed25519 EdEC Crypto"
   homepage "https://github.com/passulo/ed25519-cli/"
-  url "https://github.com/passulo/ed25519-cli/archive/refs/tags/v0.1.tar.gz"
-  sha256 "993522747a3d3acd0eb97c7d9eac4a9cd2c9cf3e6eea8e4a52a10e65d2b533d2"
+  url "https://github.com/passulo/ed25519-cli/releases/download/v0.1/ed25519-x86_64-apple-darwin"
+  sha256 "bdba8211f74478bd02a168f9dc418aaf9ea50a7434ea537a3f1bdf78934df900"
   license "Apache-2.0"
 
   depends_on "openjdk@17" => :build
-  depends_on "graalvm-ce-java17" => :build
   depends_on "sbt" => :build
 
   def install
-    system "sbt", "-sbt-jar", Formula["sbt"].opt_libexec/"bin/sbt-launch.jar", "build"
-    libexec.install "target/ed25519"
+    system "mv", "ed25519-x86_64-apple-darwin", "ed25519" 
+    bin.install "ed25519"
   end
 
   test do
